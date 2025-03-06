3c653d397cc7df66117c082787c70aa7f15176f2
heldersantiago273 <heldersantiago273@gmail.com>
2025-03-06 16:25:44 +0100

    :sparkles: Add features and shared folders

---
+++ src/app/features/calendar/components/calendar-day/calendar-day.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-calendar-day',
+  imports: [],
+  templateUrl: './calendar-day.component.html',
+  styleUrl: './calendar-day.component.scss'
+})
+export class CalendarDayComponent {
+
+}
